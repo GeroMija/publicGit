0e2f99b069da857af963899786f96e3909803942
Dmitriy <nikitin@uteka.ru>
2023-10-30 01:27:02 +0300

    add date in tests

--- cypress/e2e/desktop/01_signIn/Create_note.cy.js
+++ cypress/e2e/desktop/01_signIn/Create_note.cy.js
@@ -1,15 +1,19 @@
-let s = require('../../../storage/data_selectors.json')
+let s = require('../../../storage/data_selectors.json'),
+    date = new Date(),
+    note = `Hello world! ${date}`;
+
 describe(`Create_new_message`, function() {
-    let note = 'Hello world!';
+
     it(`Create standart new message and open`, function() {
         cy.createNewNote({ 'note': note })
         cy.getNote({ 'open': true, 'note': note })
     });
+
     it(`Create standart new message and reject to open later`, function() {
         cy.createNewNote({ 'note': note })
         cy.getNote({ 'rejection': true, 'note': note, 'reopen': true })
-        cy.getNote({ 'open': true, 'note': note })
     });
+
     it(`Create standart new message reopen old note`, function() {
         cy.createNewNote({ 'note': note })
         cy.getNote({ 'open': true, 'note': note, 'reopen': true })
